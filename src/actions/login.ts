722e9e1897a2eb37d966b80625c9d2c481826d97
AshuraDev <akogbetodonald@gmail.com>
2025-01-29 23:57:56 +0100

    feat(auth):Add user register function in register action

--- src/actions/login.ts
+++ src/actions/login.ts
@@ -6,7 +6,7 @@ import { LoginSchema } from "@/schema";
 export const login = async (values: z.infer<typeof LoginSchema>) => {
   const validatedFields = LoginSchema.safeParse(values);
 
-  if (!validatedFields) {
+  if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
 
--- src/actions/register.ts
+++ src/actions/register.ts
@@ -2,13 +2,35 @@
 
 import * as z from "zod";
 import { RegisterSchema } from "@/schema";
+import bcrypt from "bcrypt";
+import { db } from "@/lib/db";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   const validatedFields = RegisterSchema.safeParse(values);
-
-  if (!validatedFields) {
+  //validation
+  if (!validatedFields.success) {
     return { error: "Invalid fields" };
   }
+  //Extract values
+  const { name, email, password } = validatedFields.data;
 
-  return { success: "Email sent!" };
+  //Hash password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  //Verify if user already exist in database
+  const existingUser = await db.user.findUnique({ where: { email } });
+
+  //Send error message if user alredy exist
+  if (existingUser) return { error: "Email already in use!" };
+
+  //register user if user doesn't
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  return { success: "Account created!" };
 };
