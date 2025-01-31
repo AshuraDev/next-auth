409f750c220aecfeb4dba677eeb3f02e255f8c57
AshuraDev <akogbetodonald@gmail.com>
2025-01-31 12:25:47 +0100

    feat(auth):Add users role from token to session

--- src/auth.ts
+++ src/auth.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import { db } from "./lib/db";
+import { getUserById } from "./data/user";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   callbacks: {
@@ -9,9 +10,20 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
       if (token.sub && session.user) {
         session.user.id = token.sub;
       }
+
+      if (token.role && session.user) {
+        session.user.role = token.role;
+      }
       return session;
     },
     async jwt({ token }) {
+      if (!token.sub) return token;
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token;
+
+      token.role = existingUser.role;
+
       return token;
     },
   },
